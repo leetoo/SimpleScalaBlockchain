dc5f87bab975b592c4855c92a88c55e5972587b7
Robby Zambito <zambito101@gmail.com>
2018-06-10 19:08:38 -0400

    Added hash value

--- src/main/scala/com/zambito/blockchain/Block.scala
+++ src/main/scala/com/zambito/blockchain/Block.scala
@@ -4,6 +4,5 @@ import scala.compat.Platform
 
 case class Block(data: String, previousHash: String) {
   val timeStamp: Long = Platform.currentTime
-
-
+  val hash: String = (previousHash + timeStamp.toString + data).encrypt
 }
