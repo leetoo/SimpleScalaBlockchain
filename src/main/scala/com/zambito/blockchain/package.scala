4b429a701b298fa9188adbd54d67787ba3de32c7
Robby Zambito <zambito101@gmail.com>
2018-06-10 19:08:18 -0400

    Added implicit string SHA-256 encryption

--- src/main/scala/com/zambito/blockchain/package.scala
+++ src/main/scala/com/zambito/blockchain/package.scala
@@ -1,7 +1,17 @@
 package com.zambito
 
 package object blockchain {
-  implicit class StringUtil(x: String) {
+  implicit class StringUtil(str: String) {
+    import java.security.MessageDigest
+    import java.math.BigInteger
 
+    def encrypt: String =
+      String.format(
+        "%032x",
+        new BigInteger(
+          1,
+          MessageDigest.getInstance("SHA-256").digest(str.getBytes("UTF-8"))
+        )
+      )
   }
 }
