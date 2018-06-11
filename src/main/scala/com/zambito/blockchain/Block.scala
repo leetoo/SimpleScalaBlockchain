3b45b8f5f02adce0737697942f3b763ab4434b25
Robby Zambito <zambito101@gmail.com>
2018-06-11 13:24:20 -0400

    Blocks can represent themselves as XML

--- src/main/scala/com/zambito/blockchain/Block.scala
+++ src/main/scala/com/zambito/blockchain/Block.scala
@@ -1,8 +1,18 @@
 package com.zambito.blockchain
 
 import scala.compat.Platform
+import scala.xml.Elem
 
 case class Block(data: String, previousHash: String) {
   val timeStamp: Long = Platform.currentTime
   val hash: String = (previousHash + timeStamp.toString + data).encrypted()
+
+  def toXML: Elem = {
+    <block>
+      <data>{data}</data>
+      <previousHash>{previousHash}</previousHash>
+      <timeStamp>{timeStamp}</timeStamp>
+      <hash>{hash}</hash>
+    </block>
+  }
 }
--- src/main/scala/com/zambito/blockchain/Main.scala
+++ src/main/scala/com/zambito/blockchain/Main.scala
@@ -8,21 +8,11 @@ object Main extends App {
     Block("First block data", "0") #::
     Block("This is the second block", blockchain.head.hash) #::
     Block("Data for the third block", blockchain(1).hash) #::
-    Stream[Block]()
-
-  def blockToXML(block: Block) = {
-    <block>
-      <data>{block.data}</data>
-      <previousHash>{block.previousHash}</previousHash>
-      <timeStamp>{block.timeStamp}</timeStamp>
-      <hash>{block.hash}</hash>
-    </block>
-  }
-
+    Stream.empty[Block]
 
   println(
     new PrettyPrinter(195, 4).format(
-      <blockchain>{ blockchain map blockToXML }</blockchain>
+      <blockchain>{ blockchain.map(_.toXML) }</blockchain>
     )
   )
 }
