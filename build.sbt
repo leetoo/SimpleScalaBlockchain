23ab34faa279eb3003f27d8d59a0a45395416b83
Robby Zambito <zambito101@gmail.com>
2018-06-10 23:52:38 -0400

    Created JSON Blockchain

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "SimpleScalaBlockchain"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies += "com.google.code.gson" % "gson" % "2.8.5"
---
+++ src/main/scala/com/zambito/blockchain/Main.scala
@@ -0,0 +1,16 @@
+package com.zambito.blockchain
+
+import com.google.gson.GsonBuilder
+
+object Main extends App {
+
+  lazy val blockchain: Stream[Block] =
+    Block("First block data", "0") #::
+      Block("This is the second block", blockchain(0).hash) #::
+      Block("Data for the third block", blockchain(1).hash) #::
+      Stream.empty[Block]
+
+  println(
+    new GsonBuilder().setPrettyPrinting().create().toJson(scala.collection.JavaConverters.seqAsJavaList(blockchain))
+  )
+}
